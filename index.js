2f730d8d61b173f841eebf504e56cb57175271f3
Skyler Church <Pacifisity@gmail.com>
2023-12-26 17:19:56 -0500

    interest js test

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log(`python is better than JS`);
+const date = new Date();
+const hours = date.getHours();
+
+console.log(date);
+
+const dynamicAboutSection = document.getElementById('dynamic_about');
+
+if (hours >= 0 && hours <= 8) {
+	dynamicAboutSection.textContent = `🌙 Asleep`;
+} else {
+	dynamicAboutSection.textContent = `☀️ Awake`;
+}
+
+// Might not work and get transferred into user's local time, don't know how to fix that... yet
